3fe52a2a74492744d200c906c0322af028d9b9f3
Harun Richards <harun.richards@outlook.com>
2020-06-28 17:19:46 +0100

    moved javascript to its own file

---
+++ app.js
@@ -0,0 +1,9 @@
+function openSlideMenu() {
+  document.getElementById("slide-menu").style.width = '100%';
+  console.log('open');
+}
+
+function closeSlideMenu() {
+  document.getElementById("slide-menu").style.width = '0';
+  console.log('close');
+}
